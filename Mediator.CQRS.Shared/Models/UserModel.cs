b9e6c6fbbd3dbdde7cee3912d626df1b6c3b9019
huzcodes <huzcodes@outlook.com>
2024-03-12 23:31:44 +0400

    adding shared layer as part of the seperation for dcd to hold all the contracts

---
+++ Mediator.CQRS.Shared/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Mediator.CQRS.Shared.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public UserModel()
+        {
+            
+        }
+
+        public UserModel(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
