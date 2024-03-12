3e2515a5e432647a38fcc13008c93a4521243299
huzcodes <huzcodes@outlook.com>
2024-03-12 23:30:21 +0400

    adding core layer as part of the switching to dcd to hold all abstractions

---
+++ Mediator.CQRS.Core/Interfaces/IUserServices.cs
@@ -0,0 +1,11 @@
+﻿using Mediator.CQRS.Shared.Models;
+
+namespace Mediator.CQRS.Core.Interfaces
+{
+    public interface IUserServices
+    {
+        public UserModel AddUser(UserModel user);
+        public List<UserModel> GetUsers();
+        public UserModel GetUserByIndex(int index);
+    }
+}
