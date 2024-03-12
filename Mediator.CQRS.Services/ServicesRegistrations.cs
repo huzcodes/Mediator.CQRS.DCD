6bc452b3cb06429d1effd54170420c7ce8d4a32c
huzcodes <huzcodes@outlook.com>
2024-03-12 23:31:04 +0400

    adding services as part of the seperation for dcd to hold all the internal services implementations

---
+++ Mediator.CQRS.Services/ServicesRegistrations.cs
@@ -0,0 +1,14 @@
+﻿using Mediator.CQRS.Core.Interfaces;
+using Mediator.CQRS.Services.User;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mediator.CQRS.Services
+{
+    public static class ServicesRegistrations
+    {
+        public static void AddServicesRegistrations(this IServiceCollection services)
+        {
+            services.AddScoped<IUserServices, UserServices>();
+        }
+    }
+}
---
+++ Mediator.CQRS.Services/User/UserServices.cs
@@ -0,0 +1,45 @@
+﻿using Mediator.CQRS.Core.Interfaces;
+using Mediator.CQRS.Shared.Models;
+
+namespace Mediator.CQRS.Services.User
+{
+    public class UserServices : IUserServices
+    {
+        private static List<UserModel> _users = new List<UserModel>() 
+        {
+            new UserModel()
+            {
+                Id = 1,
+                FirstName = "Ahmed",
+                LastName = "Hussain"
+            },
+            new UserModel()
+            {
+                Id = 2,
+                FirstName = "John",
+                LastName = "Next"
+            },
+            new UserModel()
+            {
+                Id = 3,
+                FirstName = "Maia",
+                LastName = "Radwan"
+            },
+        };
+        public UserModel AddUser(UserModel user)
+        {
+            _users.Add(user);
+            return user;
+        }
+
+        public UserModel GetUserByIndex(int index)
+        {
+            return _users[index];
+        }
+
+        public List<UserModel> GetUsers()
+        {
+            return _users;
+        }
+    }
+}
